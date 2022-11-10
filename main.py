3e1d24cebe4ccf9f17ae985e7e5a9eb4ba86d0d7
JorgeFelixC <jorgefelix97@gmail.com>
2022-11-10 15:10:18 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from pycspade.helpers import spade, print_result
+from utility import prefixSpan, getSequences, getListFormated
+from flask import Flask, request, jsonify
+
+
+app = Flask(__name__)
+@app.route("/")
+def index():
+  sequences = getSequences('test/paths_finished.tsv')
+  wikiseed = getListFormated(sequences=sequences)
+
+  data = prefixSpan(wikiseed, 250)
+  response = jsonify({ 'data': data }) 
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  return response
+  
+
+
+
+# def initialize (): 
+#   print('This is a Formating...')
+#   sequences = getSequences('test/paths_finished.tsv')
+#   wikiseed = getListFormated(sequences=sequences)
+
+#   data = prefixSpan(wikiseed, 500)
+
+#   print(data)
+  # result = spade(filename='test/zaki.txt', support=0.3, parse=False)
+  # print(result['logger'])
+
+
+
+# if __name__ == '__main__':
+#   initialize()
+
